98938ad30d6961b72ff9dc6a84fe4b645e1d43c0
Wyatt Miller <wjmiller2016@gmail.com>
2017-01-01 11:24:50 -0500

    Added todo

--- servertest.py
+++ servertest.py
@@ -6,6 +6,8 @@ Copyright 2016, licensed by the MIT license
 Out of inspriation, I used Python to create a ping test program that runs
 on a time scheduler (i.e. cron) and reports the total ping successes or
 failures on servers/workstations that has been tested.
+
+todo: gotta make sure this program works on all operatings systems (i.e nt, posix, etc.)
 ''' 
 import os
 import sys
@@ -25,7 +27,7 @@ def pingtest(hostname, servername):
         print servername, 'is down!'
         failure += 1
 
-print "Miller Ping Test program, copyright Wyatt Miller 2016-2017"
+print "Miller Ping Test program, Copyright Wyatt Miller 2016-2017"
 
 print "Testing to see if all servers that are in your home/office are running!"
 
